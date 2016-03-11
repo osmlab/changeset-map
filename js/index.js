43c9fbd891e51248db0283db24cf3695d9008a82
Sanjay Bhangar <sanjay@mapbox.com>
2016-03-11 17:01:52 +0530

    basic permalink url handling

--- js/index.js
+++ js/index.js
@@ -1,8 +1,15 @@
 var render = require('./render');
 
+var hash = location.hash.replace('#', '');
+if (hash !== '') {
+    document.getElementById('formContainer').style.display = 'none';
+    render(hash, {});
+}
+
 document.getElementById('submitChangeset').addEventListener('click', function(e) {
     console.log('click fired');
     document.getElementById('formContainer').style.display = 'none';
     var changesetID = document.getElementById('changesetInput').value;
+    location.hash = changesetID;
     render(changesetID, {});
 });
