87eae777634158afedcd04f27db0ae137107f9f0
Ajith Ranka <ajith@mapbox.com>
2016-07-27 19:22:38 +0530

    switch overpass instance

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,7 +1,7 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var config = {
-    //'overpassBase': 'https://overpass-cfn-staging.tilestream.net/api/interpreter',
-    'overpassBase': 'https://overpass-api.de/api/interpreter',
+    'overpassBase': 'https://overpass-cfn-staging.tilestream.net/api/interpreter',
+    //'overpassBase': 'https://overpass-api.de/api/interpreter',
     'osmBase': 'https://www.openstreetmap.org/api/0.6/',
     // 'mapboxAccessToken': 'pk.eyJ1Ijoic2FuamF5YiIsImEiOiI3NjVvMFY0In0.byn_eCZGAwR1yaPeC-SVKw'
     'mapboxAccessToken': 'pk.eyJ1IjoicGxhbmVtYWQiLCJhIjoiemdYSVVLRSJ9.g3lbg_eN0kztmsfIPxa9MQ'
--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 var config = {
-    //'overpassBase': 'https://overpass-cfn-staging.tilestream.net/api/interpreter',
-    'overpassBase': 'https://overpass-api.de/api/interpreter',
+    'overpassBase': 'https://overpass-cfn-staging.tilestream.net/api/interpreter',
+    //'overpassBase': 'https://overpass-api.de/api/interpreter',
     'osmBase': 'https://www.openstreetmap.org/api/0.6/',
     // 'mapboxAccessToken': 'pk.eyJ1Ijoic2FuamF5YiIsImEiOiI3NjVvMFY0In0.byn_eCZGAwR1yaPeC-SVKw'
     'mapboxAccessToken': 'pk.eyJ1IjoicGxhbmVtYWQiLCJhIjoiemdYSVVLRSJ9.g3lbg_eN0kztmsfIPxa9MQ'
