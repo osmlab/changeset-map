685a2348bc68d74809f0e0a18f48734f1fa8bc2e
Ajith Ranka <ajith@mapbox.com>
2016-10-17 13:27:45 +0530

    addLayer() is not used

--- js/render.js
+++ js/render.js
@@ -383,31 +383,6 @@ function render(id, options) {
         return root;
     }
 
-    function addLayer(name, id) {
-        var link = document.createElement('a');
-        link.href = '#';
-        link.className = 'active';
-        link.textContent = name;
-
-        link.onclick = function (e) {
-            e.preventDefault();
-            e.stopPropagation();
-
-            var visibility = map.getLayoutProperty(id, 'visibility');
-
-            if (visibility === 'visible') {
-                map.setLayoutProperty(id, 'visibility', 'none');
-                this.className = '';
-            } else {
-                this.className = 'active';
-                map.setLayoutProperty(id, 'visibility', 'visible');
-            }
-        };
-
-        var layers = document.getElementById('menu');
-        layers.appendChild(link);
-    }
-
     function highlightFeature(featureId) {
         map.setFilter('changeset-line', [
             '==', 'id', featureId
