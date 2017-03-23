8dc1f95c61ff7209d9fa34c1a6b71e05f8716b3b
Ajith Ranka <ajith@mapbox.com>
2017-03-23 14:47:11 +0530

    change default overpass url

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 var config = {
-    'overpassBase': '//overpass-api.de/api/interpreter',
+    'overpassBase': '//overpass-cfn-production.tilestream.net/api/interpreter',
     'osmBase': '//www.openstreetmap.org/api/0.6/',
     'mapboxAccessToken': 'pk.eyJ1IjoicmFzYWd5IiwiYSI6ImNpejVrMjc4eTAwNGczM2thNWozYnJ1OHkifQ.yFRr3Sd39TJiwEguQpIkWQ'
 };
