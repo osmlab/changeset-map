af106471feccc0d87a10553e611e544869297a24
Kushan Joshi <0o3ko0@gmail.com>
2017-05-18 12:08:08 +0530

    Updates

--- js/config.js
+++
@@ -1,5 +0,0 @@
-export const config = {
-    overpassBase: '//overpass-cfn-production.tilestream.net/api/interpreter',
-    osmBase: '//www.openstreetmap.org/api/0.6/',
-    mapboxAccessToken: 'pk.eyJ1IjoicmFzYWd5IiwiYSI6ImNpejVrMjc4eTAwNGczM2thNWozYnJ1OHkifQ.yFRr3Sd39TJiwEguQpIkWQ'
-};
